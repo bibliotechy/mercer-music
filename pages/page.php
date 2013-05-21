5b083c0df91d7f4125ff56717ab285765a202b17
cbn <chadbnelson@gmail.com>
2013-05-20 21:25:36 -0400

    Add browse page views. Refactor single class into smaller units.

--- pages/page.php
+++ pages/page.php
@@ -11,14 +11,8 @@ class Page {
 	public function getData($f3) {
 		$path = (isset($this->path))? $this->path : $this->getPath($f3);
 		$request = $this->web->request($path);
-		print $request['body'];
+		//print $request['body'];
 		return json_decode($request['body']);
-	}
-	
-	public function getPath($f3) {
-		$this->path = $f3->get('apiPath') . "/" . $f3->get('PARAMS.type') . "/" . $f3->get('PARAMS.query');
-		print $this->path;
-		return $this->path;
 	}
 	
 }
--- pages/single.php
+++ pages/single.php
@@ -3,26 +3,57 @@
 class Single extends Page {
 	
 	public function renderPage($f3) {
-		$this->data = $this->getData($f3);
-		if (in_array($f3->get('PARAMS.type'), array('song', 'movie', 'show'))) {
-			$this->songdbRender($f3);
-		}
-		else {
-			$this->discographyRender($f3);
-		}
+		$this->setHeader($f3);
 		$f3->set('content','templates/' . $f3->get('PARAMS.type') . '.htm');
+		$this->data = $this->getData($f3);
+		$this->setData($f3);
 		echo $this->template->render($this->base);	
 	}
 	
-	private function songdbRender($f3) {
-	    $f3->set('data', $this->data[0]);
+	public function setHeader($f3) {
+		if (in_array($f3->get('PARAMS.type'), array('song', 'movie', 'show'))) {
+			$this->songdbHeader($f3);
+		}
+		else if ($f3->get('PARAMS.type' == 'discography')){
+			$this->discographyHeader($f3);
+		}
+		else {
+			//raise a 404
+		}
+	}
+	
+	private function songdbHeader($f3) {
 	    $f3->set('header', 'templates/songHeader.htm');
 	}
 	
-	private function discographyRender($f3) {
-		$f3->set('data', $this->data);
+	private function discographyHeader($f3) {
+		
 		$f3->set('header','templates/discographyHeader.htm');
 	}
+
+	public function setData($f3) {
+		if (in_array($f3->get('PARAMS.type'), array('song', 'movie', 'show'))) {
+			$this->setSongdbData($f3);
+		}
+		else {
+			$this->setDiscographyData($f3);
+		}
+	}
+		
+	private function setSongdbData($f3) {
+		$f3->set('data', $this->data[0]);
+	}
+	
+	private function setDiscographyData($f3) {
+		$f3->set('data', $this->data);
+	}
+	
+	public function getPath($f3) {
+		$this->path = $f3->get('apiPath') . "/" . $f3->get('PARAMS.type') . "/" . $f3->get('PARAMS.query');
+		//print $this->path;
+		return $this->path;
+	}
+
 }
 
 ?>
